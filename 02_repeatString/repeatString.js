58e83d5bd69dafd978c44466868d03f455ae45c2
Johnny Kortbawi <anjo2.johnny@gmail.com>
2024-08-27 13:57:33 +0400

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, numOfRepetitions) {
 
+  if(numOfRepetitions<0) {
+    return "ERROR";
+  }
+
+  newStr = '';
+  for (let i=0; i<numOfRepetitions; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
