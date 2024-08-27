bc4516f9c9cf1e672142f1b0a42380af8df5a047
Johnny Kortbawi <anjo2.johnny@gmail.com>
2024-08-27 18:48:48 +0400

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayToSum) {
+	sumOfArray = 0;
+  let i = 0;
+  while (i < arrayToSum.length){
+    sumOfArray += arrayToSum[i];
+    i++;
+  }
+  return sumOfArray;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayToMultiply) {
+  productOfArray = 1;
+  let i = 0;
+  while (i < arrayToMultiply.length){
+    productOfArray *= arrayToMultiply[i];
+    i++;
+  }
+  return productOfArray;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	powerNum = num;
+  for (let i=0; i<exponent-1;i++) {
+    powerNum *= num;
+  }
+  return powerNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	factorialNum = 1;
+  while (num > 0) {
+    factorialNum *= num;
+    num--;
+  }
+  return factorialNum;
 };
 
 // Do not edit below this line
