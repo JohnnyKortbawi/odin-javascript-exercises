685ef12a959407bd88f4167a385bd7c429d4fecc
Johnny Kortbawi <anjo2.johnny@gmail.com>
2024-08-29 13:06:46 +0400

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Since we only consider letters and numbers, create a variable containing all valid characters
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+  const cleanedString = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+  let i = 0;
+  let j = cleanedString.length - 1;
+
+  while (i<j) {
+    if (cleanedString[i]!=cleanedString[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
 };
 
 // Do not edit below this line
