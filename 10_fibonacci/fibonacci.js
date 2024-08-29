2b97c6076d930c0be4501138935a3cf21760a376
Johnny Kortbawi <anjo2.johnny@gmail.com>
2024-08-29 13:29:11 +0400

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (typeof(number) == 'string') {
+    number = number.toString();
+  }
+  if (number < 0) {return "OOPS";}
+  else if (number == 0) {return 0;}
+  else {
+    num1 = 0;
+    num2 = 1;
+    let fib = 1;
+    for (let i=1; i<number; i++) {
+      fib = num1 + num2;
+      num1 = num2;
+      num2 = fib;
+    }
+    return fib;
+  }
 };
 
 // Do not edit below this line
